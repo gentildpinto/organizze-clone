1949fbf6988e1335c34fe800e2b4f2103146a91f
gentildpinto <gentil.junior.gp@gmail.com>
2022-09-26 13:44:51 +0100

    added signIn and signUp activities

--- app/src/main/java/com/example/organizze/IntroActivity.java
+++ app/src/main/java/com/example/organizze/IntroActivity.java
@@ -1,7 +1,14 @@
 package com.example.organizze;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.organizze.activity.SignInActivity;
+import com.example.organizze.activity.SignUpActivity;
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
 public class IntroActivity extends com.heinrichreimersoftware.materialintro.app.IntroActivity {
@@ -9,7 +16,6 @@ public class IntroActivity extends com.heinrichreimersoftware.materialintro.app.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
         setButtonNextVisible(false);
         setButtonBackVisible(false);
@@ -32,8 +38,21 @@ public class IntroActivity extends com.heinrichreimersoftware.materialintro.app.
         addSlide(new FragmentSlide.Builder()
                 .fragment(R.layout.intro_four)
                 .background(android.R.color.white)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .fragment(R.layout.intro_auth)
+                .background(android.R.color.white)
                 .canGoForward(false)
                 .build()
         );
+    }
+
+    public void signUp(View view) {
+        startActivity(new Intent(this, SignUpActivity.class));
+    }
+
+    public void signIn(View view) {
+        startActivity(new Intent(this, SignInActivity.class));
     }
 }
